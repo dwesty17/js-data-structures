552ff590f25d316220a1777db27c9149bc7f2fc5
dwesty17 <j.dylan.westerhold@gmail.com>
2019-05-29 18:12:51 -0700

    Added BinaryTreeNode class

---
+++ src/BinaryTreeNode.js
@@ -0,0 +1,12 @@
+// TODO create Node (has data) class to extend here
+class BinaryTreeNode {
+
+  constructor (data, leftChild, rightChild) {
+    this.data = data;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+  }
+
+}
+
+module.exports = BinaryTreeNode;
