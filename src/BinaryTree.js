1eff40f5c0dcb289992106072f3340fc3c51929e
dwesty17 <j.dylan.westerhold@gmail.com>
2019-05-29 17:49:54 -0700

    Added traversal methods for BinaryTree class

---
+++ src/BinaryTree.js
@@ -0,0 +1,25 @@
+class BinaryTree {
+
+  constructor (root) {
+    this.root = root;
+  }
+
+  inOrderTraverse (callback, node = this.root) {
+    callback(node);
+    node.leftChild && this.inOrderTraverse(callback, this.root.leftChild);
+    node.rightChild && this.inOrderTraverse(callback, this.root.rightChild);
+  }
+
+  preOrderTraverse (callback, node = this.root) {
+    callback(node);
+    node.leftChild && this.preOrderTraverse(callback, this.root.leftChild);
+    node.rightChild && this.preOrderTraverse(callback, this.root.rightChild);
+  }
+
+  postOrderTraverse (callback, node = this.root) {
+    node.leftChild && this.postOrderTraverse(callback, this.root.leftChild);
+    node.rightChild && this.postOrderTraverse(callback, this.root.rightChild);
+    callback(node);
+  }
+
+}
