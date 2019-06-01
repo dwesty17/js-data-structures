f1b28593c0570db0f2ce61aef58bb78a9c2b6db3
dwesty17 <j.dylan.westerhold@gmail.com>
2019-06-01 16:19:16 -0600

    Implemented tests

--- src/MaxHeap.test.js
+++ src/MaxHeap.test.js
@@ -8,8 +8,24 @@ describe("MaxHeap", () => {
     maxHeap = new MaxHeap();
   });
 
-  test("Returns undefined if the tree is empty", () => {});
+  test("Returns undefined if the tree is empty", () => {
+    const max = maxHeap.extract();
+    expect(max).toBeUndefined();
+  });
 
-  describe("extract returns the maximum element", () => {});
+  test("extract returns the maximum element", () => {
+    maxHeap.insert(new BinaryTreeNode(3));
+    maxHeap.insert(new BinaryTreeNode(4));
+    maxHeap.insert(new BinaryTreeNode(1));
+    maxHeap.insert(new BinaryTreeNode(5));
+    maxHeap.insert(new BinaryTreeNode(2));
+
+    expect(maxHeap.extract()).toBe(5);
+    expect(maxHeap.extract()).toBe(4);
+    expect(maxHeap.extract()).toBe(3);
+    expect(maxHeap.extract()).toBe(2);
+    expect(maxHeap.extract()).toBe(1);
+    expect(maxHeap.extract()).toBeUndefined();
+  });
 
 });
--- src/MinHeap.test.js
+++ src/MinHeap.test.js
@@ -8,8 +8,24 @@ describe("MinHeap", () => {
     minHeap = new MinHeap();
   });
 
-  test("Returns undefined if the tree is empty", () => {});
+  test("Returns undefined if the tree is empty", () => {
+    const min = minHeap.extract();
+    expect(min).toBeUndefined();
+  });
 
-  describe("extract returns the minimum element", () => {});
+  test("extract returns the minimum element", () => {
+    minHeap.insert(new BinaryTreeNode(3));
+    minHeap.insert(new BinaryTreeNode(4));
+    minHeap.insert(new BinaryTreeNode(1));
+    minHeap.insert(new BinaryTreeNode(5));
+    minHeap.insert(new BinaryTreeNode(2));
+
+    expect(minHeap.extract()).toBe(1);
+    expect(minHeap.extract()).toBe(2);
+    expect(minHeap.extract()).toBe(3);
+    expect(minHeap.extract()).toBe(4);
+    expect(minHeap.extract()).toBe(5);
+    expect(minHeap.extract()).toBeUndefined();
+  });
 
 });
